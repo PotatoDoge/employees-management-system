6874e2e5c7398c7f3efdb6164850fd3dbdcf15a1
bprado <bastian.prado.v@gmail.com>
2023-09-12 19:27:53 -0700

    Change response types

--- src/main/java/com/bfp/employeesmanagementsystem/controller/EmployeeController.java
+++ src/main/java/com/bfp/employeesmanagementsystem/controller/EmployeeController.java
@@ -1,35 +1,45 @@
 package com.bfp.employeesmanagementsystem.controller;
 
+import com.bfp.employeesmanagementsystem.dto.EmployeeDto;
+import com.bfp.employeesmanagementsystem.entity.Employee;
+import com.bfp.employeesmanagementsystem.service.EmployeeService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("employees")
 @AllArgsConstructor
 public class EmployeeController {
 
-    @GetMapping("employees")
-    public String getEmployees(){
-        return "welcome from employees";
+    private final EmployeeService employeeService;
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getEmployees(){
+        return ResponseEntity.ok(employeeService.getEmployees());
     }
 
-    @GetMapping("employees/{id}")
-    public String getEmployeeById(@PathVariable("id") Long id){
-        return "welcome from employees: " + id;
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable("id") Long id){
+        return ResponseEntity.ok(employeeService.getEmployeeById(id));
     }
 
-    @PostMapping("employees")
-    public String createEmployee(){
-        return "Create employee";
+    @PostMapping
+    public ResponseEntity<Employee> createEmployee(@RequestBody EmployeeDto employeeDto){
+        return ResponseEntity.ok(employeeService.createEmployee(employeeDto));
     }
 
-    @PutMapping("employees/{id}")
-    public String updateEmployee(){
-        return "Update employee";
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> updateEmployee(@PathVariable("id") Long id){
+        return ResponseEntity.ok(employeeService.updateEmployee(id));
     }
 
-    @DeleteMapping("employees/{id}")
-    public String deleteEmployee(){
-        return "Delete employee";
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long id){
+        employeeService.deleteEmployee(id);
+        return ResponseEntity.ok("Deleted! :D");
     }
 
 }
