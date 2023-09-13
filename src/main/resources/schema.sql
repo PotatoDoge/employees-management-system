b444010eac80bc6c77dbf82f8b06b256c9e83f2b
bprado <bastian.prado.v@gmail.com>
2023-09-12 19:27:28 -0700

    Change columns names

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -9,7 +9,8 @@ create table roles(
 DROP TABLE IF EXISTS employees;
 create table employees(
   id int not null AUTO_INCREMENT,
-  name varchar(100) not null,
+  first_name varchar(100) not null,
+  last_name varchar(100) not null,
   role int not null,
   PRIMARY KEY (id),
   FOREIGN KEY(role) REFERENCES roles(id) ON DELETE CASCADE ON UPDATE CASCADE
