9eeee974b20fe40b0936e47063f990b2ba91c112
bprado <bastian.prado.v@gmail.com>
2023-09-12 19:27:40 -0700

    Add EmployeeDto class

---
+++ src/main/java/com/bfp/employeesmanagementsystem/dto/EmployeeDto.java
@@ -0,0 +1,11 @@
+package com.bfp.employeesmanagementsystem.dto;
+
+import jakarta.persistence.Id;
+
+public class EmployeeDto {
+
+    private String firstName;
+    private String lastName;
+    private String role;
+
+}
