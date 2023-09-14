556949044d134e0051b82ec6e5450ff8c677ef3d
bprado <bastian.prado.v@gmail.com>
2023-09-13 20:35:12 -0700

    Add setter and getter to EmployeeDto

--- src/main/java/com/bfp/employeesmanagementsystem/dto/EmployeeDto.java
+++ src/main/java/com/bfp/employeesmanagementsystem/dto/EmployeeDto.java
@@ -1,7 +1,11 @@
 package com.bfp.employeesmanagementsystem.dto;
 
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class EmployeeDto {
 
     private String firstName;
