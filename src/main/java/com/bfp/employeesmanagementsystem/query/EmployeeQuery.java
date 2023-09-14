32fed64b0c51047b0d5d048d5da30a0185f0de50
bprado <bastian.prado.v@gmail.com>
2023-09-13 22:08:37 -0700

    Add update query

--- src/main/java/com/bfp/employeesmanagementsystem/query/EmployeeQuery.java
+++ src/main/java/com/bfp/employeesmanagementsystem/query/EmployeeQuery.java
@@ -5,5 +5,6 @@ public class EmployeeQuery {
     public static final String GET_EMPLOYEE_BY_ID = "SELECT id, first_name, last_name, role FROM employees WHERE id=:id";
     public static final String GET_EMPLOYEES = "SELECT id, first_name, last_name, role from employees";
     public static final String CREATE_EMPLOYEE = "INSERT INTO employees (first_name, last_name, role) values (:firstName, :lastName, :role)";
+    public static final String UPDATE_EMPLOYEE = "UPDATE employees set first_name = :firstName, last_name = :lastName, role = :role where id = :id";
 
 }
