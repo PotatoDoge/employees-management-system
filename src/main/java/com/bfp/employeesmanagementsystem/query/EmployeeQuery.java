c698e61e7254b0b7d00f57224fed26cd0b35ebf7
bprado <bastian.prado.v@gmail.com>
2023-09-17 22:25:15 -0700

    Add delete employee query

--- src/main/java/com/bfp/employeesmanagementsystem/query/EmployeeQuery.java
+++ src/main/java/com/bfp/employeesmanagementsystem/query/EmployeeQuery.java
@@ -6,5 +6,5 @@ public class EmployeeQuery {
     public static final String GET_EMPLOYEES = "SELECT id, first_name, last_name, role from employees";
     public static final String CREATE_EMPLOYEE = "INSERT INTO employees (first_name, last_name, role) values (:firstName, :lastName, :role)";
     public static final String UPDATE_EMPLOYEE = "UPDATE employees set first_name = :firstName, last_name = :lastName, role = :role where id = :id";
-
+    public static final String DELETE_EMPLOYEE = "DELETE FROM employees where id = :id";
 }
